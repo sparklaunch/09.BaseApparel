2faf7b364c7f336861a21b824fde8567445a1822
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-20 11:10:27 +0900

    first commit

---
+++ base-apparel/src/App.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600&display=swap"
+        rel="stylesheet"
+    />
+</svelte:head>
+<div id="app" />
