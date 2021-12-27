d2eb74fc68524b1c94364d54dc498a9aa083ac15
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-28 08:58:17 +0900

    Add the logo

--- base-apparel/src/components/Left.svelte
+++ base-apparel/src/components/Left.svelte
@@ -1,7 +1,21 @@
-<div id="left" />
+<script lang="ts">
+    import Logo from "./Logo.svelte";
+    import Content from "./Content.svelte";
+    import Form from "./Form.svelte";
+</script>
+
+<div id="left">
+    <Logo />
+    <Content />
+    <Form />
+</div>
 
 <style>
     #left {
         width: 55%;
+        background-image: url("/assets/bg-pattern-desktop.svg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 50px 100px;
     }
 </style>
---
+++ base-apparel/src/components/Logo.svelte
@@ -0,0 +1,9 @@
+<div id="logo">
+    <img src="/assets/logo.svg" alt="Logo" />
+</div>
+
+<style>
+    #logo {
+        margin-bottom: 100px;
+    }
+</style>
